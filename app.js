b69aafb1d9c05b68960e07091abb9eb1a0ca44f7
hopshackle <james@janigo.co.uk>
2017-01-21 20:46:34 +0000

    Modified dev and prod settings to use NODE_ENV

--- app.js
+++ app.js
@@ -10,37 +10,49 @@ var users = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+/**
+ * Development Settings
+ */
+if (app.get('env') === 'development') {
+    // This will change in production since we'll be using the public folder
+    app.use(express.static(path.join(__dirname, '../am-client')));
+    // This covers serving up the index page
+    app.use(express.static(path.join(__dirname, '../am-client/.tmp')));
+    app.use(express.static(path.join(__dirname, '../am-client/app')));
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+    // Error Handling
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+/**
+ * Production Settings
+ */
+if (app.get('env') === 'production') {
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // changes it to use the optimized version for production
+    app.use(express.static(path.join(__dirname, 'public')));
 
+    // production error handler
+    // no stacktraces leaked to user
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: {}
+        });
+    });
+}
 module.exports = app;

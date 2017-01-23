46900096477d4d5541a54df50c01f0ddf548289c
hopshackle <james@janigo.co.uk>
2017-01-23 12:07:55 +0000

    AddSeason now works

--- api/seasonalActivity.js
+++ api/seasonalActivity.js
@@ -2,6 +2,23 @@
 var Season = require('./models/season');
 var util = require('../controllers/utilities');
 
+function convertItemsUsed(items) {
+    if (items) return items.split(",");
+    return ""
+}
+
+function createCompliantObjectFrom(req) {
+    return {
+        year: parseInt(req.body.year),
+        season: parseInt(req.body.season),
+        magus: req.body.magus,
+        description: req.body.description,
+        isService: req.body.isService,
+        itemsUsed: convertItemsUsed(req.body.itemsUsed),
+        serviceForMagus: req.body.serviceForMagus
+    }
+}
+
 module.exports = {
 
     covenantData: function (req, res) {
@@ -14,7 +31,10 @@ module.exports = {
         // delete a given record
     },
     addSeason: function (req, res) {
-        // add details of a new season
+        Season.create(createCompliantObjectFrom(req), function(err, newRecord) {
+            if (err) util.sendJsonResponse(res, 400, err);
+            util.sendJsonResponse(res, 200, newRecord);
+        });
     }
 
 }

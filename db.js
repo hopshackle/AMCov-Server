b3093415ff086e8ed4aad5def115012c988243a9
James Goodman <james@janigo.co.uk>
2017-10-05 12:57:58 +0100

    added production mongodb link

--- db.js
+++ db.js
@@ -4,8 +4,10 @@ var dbURI = 'mongodb://bollocks/ArsMagicaCovenants';
 var ENV = process.env.NODE_ENV.trim();
 switch (ENV) {
     case 'production':
+        dbURI = 'mongodb://heroku_qq3kz0c4:42dpds0idhh48i2tg147r13emj@ds161194.mlab.com:61194/heroku_qq3kz0c4';
+        break;
     case 'development':
         dbURI = 'mongodb://localhost/ArsMagicaCovenants';
-        console.log("uri is " + dbURI);
 }
+console.log("uri is " + dbURI);
 mongoose.connect(dbURI);
